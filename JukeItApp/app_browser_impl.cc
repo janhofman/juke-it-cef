bda5189245603f5e498d2c6cf2d5c83747fa1f7f
Michal Jurčo <michal.jurco007@gmail.com>
2018-03-23 15:03:49 +0100

    MessageRouter example code made to work, should be possible to write own code now

---
+++ JukeItApp/app_browser_impl.cc
@@ -0,0 +1,47 @@
+#include "client_impl.h"
+#include "app_factory.h"
+#include "browser_util.h"
+#include "resource_util.h"
+
+namespace message_router {
+
+	namespace {
+
+		std::string GetStartupURL() {
+			return shared::kTestOrigin + std::string("message_router.html");
+		}
+
+	}  // namespace
+
+	   // Implementation of CefApp for the browser process.
+	class BrowserApp : public CefApp, public CefBrowserProcessHandler {
+	public:
+		BrowserApp() {}
+
+		// CefApp methods:
+		CefRefPtr<CefBrowserProcessHandler> GetBrowserProcessHandler() OVERRIDE {
+			return this;
+		}
+
+		// CefBrowserProcessHandler methods:
+		void OnContextInitialized() OVERRIDE {
+			// Create the browser window.
+			const CefString& startup_url = GetStartupURL();
+			shared::CreateBrowser(new Client(startup_url), startup_url,
+				CefBrowserSettings());
+		}
+
+	private:
+		IMPLEMENT_REFCOUNTING(BrowserApp);
+		DISALLOW_COPY_AND_ASSIGN(BrowserApp);
+	};
+
+}  // namespace message_router
+
+namespace shared {
+
+	CefRefPtr<CefApp> CreateBrowserProcessApp() {
+		return new message_router::BrowserApp();
+	}
+
+}  // namespace shared
---
+++ JukeItApp/app_browser_impl.h
@@ -0,0 +1,8 @@
+//#pragma once
+//class app_browser_impl
+//{
+//public:
+//	app_browser_impl();
+//	~app_browser_impl();
+//};
+//
